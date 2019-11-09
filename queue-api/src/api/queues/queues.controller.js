7e54896d0d33fa3256eaa9c93c4e3242ee202708
natthawat <banz_222@windowslive.com>
2019-11-09 20:57:09 +0700

    9/11v.1

--- queue-api/src/api/queues/queues.controller.js
+++ queue-api/src/api/queues/queues.controller.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import loadDB from '../../config/db'
+import moment from 'moment'
 
 const router = express.Router()
 
@@ -14,18 +15,21 @@ const index = async (req, res, next) => {
 const create = async (req, res, next) => {
   const { comment } = req.body
   const db = await loadDB()
-  await db.query(`SELECT queueNumber, technicians__id FROM queues ORDER BY id DESC LIMIT 1; SELECT id, name, role, status FROM users WHERE role = 'technician' AND status = 1; SELECT id FROM users WHERE username = '${req.decoded.username}';`, async (err, results) => {
+  await db.query(`SELECT queueNumber, createdAt, technicians__id FROM queues ORDER BY id DESC LIMIT 1; SELECT id, name, role, status FROM users WHERE role = 'technician' AND status = 1; SELECT id FROM users WHERE username = '${req.decoded.username}';`, async (err, results) => {
     if (err) throw err
     const userId = results[2][0].id
     let queueNumber, technicians__id
-    let nextId
     let techniciansObj = JSON.parse(JSON.stringify(results[1]))
-    if (technicians__id)
-      nextId = techniciansObj.findIndex((tec) => tec.id == results[0][0].technicians__id)
     if (results[0].length === 0) {
       queueNumber = 1
       technicians__id = results[1][0].id
     } else {
+      let nextId
+      if (moment(results[0][0].createdAt).format("YYYY-MM-DD") === moment(new Date()).format("YYYY-MM-DD")) {
+        nextId = techniciansObj.findIndex((tec) => tec.id == results[0][0].technicians__id)
+      } else {
+        nextId = 1
+      }
       queueNumber = Number(results[0][0].queueNumber) + 1
       if (nextId < techniciansObj.length - 1)
         technicians__id = results[1][nextId + 1].id
