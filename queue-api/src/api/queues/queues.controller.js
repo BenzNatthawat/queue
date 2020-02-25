f618d907da8ea75301595305cb70ae1f1be85b2f
natthawat <banz_222@windowslive.com>
2020-02-25 17:19:41 +0700

    25/2v.1

--- queue-api/src/api/queues/queues.controller.js
+++ queue-api/src/api/queues/queues.controller.js
@@ -14,62 +14,68 @@ const index = async (req, res, next) => {
 
 const create = async (req, res, next) => {
   const { comment, insurance, jobnumber } = req.body
+  const { id } = req.decoded
   const db = await loadDB()
 
-  await db.query(`SELECT queueNumber, createdAt, technicians__id FROM queues ORDER BY id DESC LIMIT 1; SELECT id, name, role, status FROM users WHERE role = 'technician' AND status = 1; SELECT id FROM users WHERE username = '${req.decoded.username}';`, async (err, results) => {
+  await db.query(`
+    SELECT queueNumber, createdAt, technicians__id FROM queues ORDER BY id DESC LIMIT 1;
+    SELECT users.id, IFNULL(groupQueues.countTechniciansId, 0) AS queuesLoad
+    FROM users
+    LEFT OUTER JOIN 
+      (SELECT technicians__id, COUNT(technicians__id) AS countTechniciansId FROM queues WHERE STATUS = 'wait' GROUP BY technicians__id) AS groupQueues
+    ON users.id = groupQueues.technicians__id
+    WHERE role = 'technician' AND STATUS = 1;
+    SELECT id FROM users WHERE id = ${id};
+  `, async (err, results) => {
     if (err) throw err
-    const userId = results[2][0].id
-    let queueNumber, technicians__id
-    let techniciansObj = JSON.parse(JSON.stringify(results[1]))
-    
-    console.log(results)
-
-    if (results[1].length >= 1) {
+    let queueNumber, techniciansId, minQueueLoad
+    if (results[1].length > 0) {
       if (results[0].length === 0) {
         queueNumber = 1
-        technicians__id = results[1][0].id
+        techniciansId = results[1][0].id
       } else {
-        let nextId
-        if (moment(results[0][0].createdAt).format("YYYY-MM-DD") === moment(new Date()).format("YYYY-MM-DD")) {
-          nextId = techniciansObj.findIndex((tec) => tec.id == results[0][0].technicians__id)
-          queueNumber = Number(results[0][0].queueNumber) + 1
-        } else {
-          nextId = -1
+        //    หาว่าช่างคนไหน load queue น้อย สุด
+        minQueueLoad = results[1].reduce(function (res, obj) {
+          return (obj.queuesLoad < res.queuesLoad) ? obj : res;
+        })
+        techniciansId = minQueueLoad.id
+        if (moment(results[0][0].createdAt).format("YYYY-MM-DD") === moment(new Date()).format("YYYY-MM-DD"))
+          queueNumber = parseInt(results[0][0].queueNumber) + 1
+        else
           queueNumber = 1
-        }
-        if (nextId < techniciansObj.length - 1)
-          technicians__id = results[1][nextId + 1].id
-        else {
-          technicians__id = results[1][0].id
-        }
       }
-      console.log(results)
-      // await db.query(`INSERT INTO queues (queueNumber, comment, technicians__id, users__id, insurance, jobnumber) VALUES (${queueNumber}, '${comment}', ${technicians__id}, ${userId}, '${insurance || ''}', '${jobnumber || ''}')`, async (err, results) => {
-      //   if (err) throw err
-      //   await db.query(`SELECT queues.queueNumber, queues.comment, users.name FROM queues INNER JOIN users ON users.id = queues.technicians__id WHERE queues.id = ${results.insertId}`, async (err, results) => {
-      //     if (err) throw err
-      //     return res.json(results[0])
-      //   })
-      // })
     } else {
       return res.json({ error: 'กรุณาเพิ่มช่าง' })
     }
+
+    await db.query(`INSERT INTO queues (queueNumber, comment, technicians__id, users__id, insurance, jobnumber) VALUES (${queueNumber}, '${comment}', ${techniciansId}, ${id}, '${insurance || ''}', '${jobnumber || ''}')`, async (err, results) => {
+      if (err) throw err
+      await db.query(`SELECT queues.queueNumber, queues.comment, users.name FROM queues INNER JOIN users ON users.id = queues.technicians__id WHERE queues.id = ${results.insertId}`, async (err, results) => {
+        if (err) throw err
+        return res.json(results[0])
+      })
+    })
   })
 }
 
 const show = async (req, res, next) => {
   const { id } = req.decoded
   const db = await loadDB()
-  await db.query(`SELECT queueNumber, technicians__id FROM queues WHERE technicians__id = ${id} ORDER BY id DESC LIMIT 4`, async (err, results) => {
+  await db.query(`
+    SELECT id, queueNumber, technicians__id, status, insurance FROM queues 
+    WHERE technicians__id = 12 AND status = 'wait'
+    ORDER BY insurance, id LIMIT 4
+  `, async (err, results) => {
     if (err) throw err
     return res.json(results)
   })
 }
 
 const update = async (req, res, next) => {
-  const { id } = req.decoded
+  const { status } = req.body
+  const { id } = req.params
   const db = await loadDB()
-  await db.query(`SELECT queueNumber, technicians__id FROM queues WHERE technicians__id = ${id} ORDER BY id DESC LIMIT 4`, async (err, results) => {
+  await db.query(`UPDATE queues SET status = '${status}' WHERE id = ${id};`, async (err, results) => {
     if (err) throw err
     return res.json(results)
   })
