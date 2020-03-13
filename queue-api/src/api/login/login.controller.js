df6d45e16162f782dc56fedea3fdb3f9e5983d9b
natthawat <banz_222@windowslive.com>
2020-03-13 10:04:56 +0700

    13/3v.1

--- queue-api/src/api/login/login.controller.js
+++ queue-api/src/api/login/login.controller.js
@@ -8,24 +8,28 @@ const router = express.Router()
 const login = async (req, res, next) => {
   const { username, password } = req.body
   const db = await loadDB()
-  console.log('login' + username)
-  if (username && password) {
-    const user = await db.query(`SELECT * FROM users WHERE username = '${username}'`, async (err, results) => {
-      if (err) throw err
-      if (results.length > 0) {
-        const loginStatus = bcrypt.compareSync(password, results[0].password)
-        if (loginStatus) {
-          const resultsLogin = results[0]
-          const user = await db.query(`UPDATE users SET status=1 WHERE id=${resultsLogin.id}`, async (err, results) => {
-            if (err) throw err
-            const token = signin({ id: resultsLogin.id, username })
-            return res.json({ success: 'login success', token, username: resultsLogin.username, name: resultsLogin.name, role: resultsLogin.role })
-          })
+  console.log('login : ' + username)
+  try {
+    if (username && password) {
+      const user = await db.query(`SELECT * FROM users WHERE username = '${username}'`, async (err, results) => {
+        if (err) throw err
+        if (results.length > 0) {
+          const loginStatus = bcrypt.compareSync(password, results[0].password)
+          if (loginStatus) {
+            const resultsLogin = results[0]
+            const user = await db.query(`UPDATE users SET status=1 WHERE id=${resultsLogin.id}`, async (err, results) => {
+              if (err) throw err
+              const token = signin({ id: resultsLogin.id, username })
+              return res.json({ success: 'login success', token, username: resultsLogin.username, name: resultsLogin.name, role: resultsLogin.role })
+            })
+          }
         }
-      }
-    })
-  } else {
-    return res.json({ error: 'required', username, password })
+      })
+    } else {
+      return res.json({ error: 'required', username, password })
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
